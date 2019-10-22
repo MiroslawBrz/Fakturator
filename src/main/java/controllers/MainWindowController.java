3478082458b59540ee6903c5b8feae1ed39b4d92
MiroslawBrz <miroslawbrzezinski@gmail.com>
2019-10-22 21:13:49 +0200

    NewWindow

--- src/main/java/controllers/MainWindowController.java
+++ src/main/java/controllers/MainWindowController.java
@@ -1,8 +1,19 @@
 package controllers;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainWindowController {
 
-    public void addClientWindow() {
-
+    public void addClientWindow() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxmlFiles/ClientWindow.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Wprowadź dane klienta");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
